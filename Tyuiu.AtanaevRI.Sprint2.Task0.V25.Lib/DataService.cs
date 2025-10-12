e92b2870b487ea0d3885929725e76be1caeb8c45
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-10-12 13:41:49 +0500

    Add project files.

---
+++ Tyuiu.AtanaevRI.Sprint2.Task0.V25.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.AtanaevRI.Sprint2.Task0.V25.Lib
+{
+    public class DataService : ISprint2Task0V25
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+            res[0] = x == y + 46;
+            res[1] = x != y;
+            res[2] = x < y + 100;
+            res[3] = x > y;
+            res[4] = x <= y + 200;
+            res[5] = x >= y;
+            return res;
+
+        }
+    }
+}
+
+
+
+
---
+++ Tyuiu.AtanaevRI.Sprint2.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.AtanaevRI.Sprint2.Task0.V25.Lib;
+namespace Tyuiu.AtanaevRI.Sprint2.Task0.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 205;
+            int y = 159;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { true, true, true, true, true, true };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
