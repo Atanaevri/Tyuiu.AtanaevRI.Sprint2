c5f05865db36c282f31be461157ec9d379efd268
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-10-12 18:42:17 +0500

    Task1

---
+++ Tyuiu.AtanaevRI.Sprint2.Task1.V25/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.AtanaevRI.Sprint2.Task1.V25.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int a = 247;
+        int b = 155;
+        int c = 325;
+        int d = 857;
+        bool[] res = new bool[6];
+        res = ds.GetLogicOperations(a, b, c, d);
+
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+        Console.WriteLine("c = " + c);
+        Console.WriteLine("d = " + d);
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+        Console.ReadKey();
+    }
+}
