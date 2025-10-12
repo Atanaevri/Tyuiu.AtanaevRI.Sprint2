c1bcd5a51e039458f1a3b50c56ddfe9260706424
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-10-12 21:03:08 +0500

    Task2

---
+++ Tyuiu.AtanaevRI.Sprint2.Task2.V0.Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.AtanaevRI.Sprint2.Task2.V0.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tyuiu.AtanaevRI.Sprint2.Task2.V30/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.AtanaevRI.Sprint2.Task2.V30.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+       
+
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Введите Значение Х");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(" Введите Значение Y");
+        int y = Convert.ToInt32(Console.ReadLine());
+       
+        bool res = ds.CheckDotInShadedArea(x, y);
+      
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        if (res)
+        {
+            Console.WriteLine("Точка находится в заштрихованной области");
+        }
+        else
+        {
+            Console.WriteLine("Точка не находится в заштрихованной области");
+        }
+            Console.ReadKey();
+    }
+}
