60bad49f1c7a89760b08f06bd0b3bd9806d9d2f7
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-10-14 14:57:12 +0500

    Task3

---
+++ Tyuiu.AtanaevRI.Sprint2.Task3.V30/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.AtanaevRI.Sprint2.Task3.V30.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Введите Значение Х");
+        int x = Convert.ToInt32(Console.ReadLine());
+      
+
+        double res = ds.Calculate(x);
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+       
+        Console.WriteLine("Значение функции="+res);
+       
+    }
+}
