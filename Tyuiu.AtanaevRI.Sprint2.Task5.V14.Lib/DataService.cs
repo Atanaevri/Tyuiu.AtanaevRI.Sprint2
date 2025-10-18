5ad6373219dd7cc6339a179d06cb8ff189706648
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-10-18 11:29:51 +0500

    Task5

--- Tyuiu.AtanaevRI.Sprint2.Task5.V14.Lib/DataService.cs
+++ Tyuiu.AtanaevRI.Sprint2.Task5.V14.Lib/DataService.cs
@@ -1,7 +1,8 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint2;
+﻿
+using tyuiu.cources.programming.interfaces.Sprint2;
 namespace Tyuiu.AtanaevRI.Sprint2.Task5.V14.Lib
 {
-    public class DataService : ISprint2Task5V14
+    public class DataService
     {
         public string FindDayName(int k)
         {
